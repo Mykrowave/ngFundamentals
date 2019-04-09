2f2b5293350f7762653710c073f8881289ffe0b1
Mike Parker <mikeparker@crescerance.com>
2019-04-09 15:39:56 -0400

    ExtraCreditOn Menu

--- src/app/nav/nav-bar/nav-bar.component.ts
+++ src/app/nav/nav-bar/nav-bar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/user/auth.service';
 import { EventService } from 'src/app/events/shared/event.service';
 import { ISession } from 'src/app/events/shared/isession';
+import { IEvent } from 'src/app/events/shared/ievent';
+import { EventsListComponent } from 'src/app/events/events-list/events-list.component';
 
 @Component({
   selector: 'app-nav-bar',
@@ -12,9 +14,15 @@ export class NavBarComponent implements OnInit {
 
   searchTerm = '';
   foundSessions: ISession[];
+  events: IEvent[];
   constructor(public authService: AuthService, private eventService: EventService) { }
 
   ngOnInit() {
+    this.eventService.getEvents().forEach(data => {
+      this.events = data;
+    });
+
+    console.log(this.events);
   }
 
   searchSessions(searchTerm: string) {
